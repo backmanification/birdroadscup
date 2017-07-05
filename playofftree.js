7d181ec986b7330e3b09e9f775b8e6203d75d6c3
Filip <fbackman@cern.ch>
2017-07-06 00:32:48 +0200

    funktionell hemsida

--- playofftree.js
+++ playofftree.js
@@ -21,7 +21,7 @@ var bracketEast = {
       [
           [[0, 1,'Match 1'], [2, 3, 'Match 2'], [4, 5, 'Match 3'], [6, 7, 'Match 4']],
           [[8, 9,'Atlantic Division Final'], [10, 11, ' Division Final']],
-          [[12, 13, 'Western Conference Final'], [null, null, 'game']]
+          [[12, 13, 'Western Conference Final']]
       ]
   ]
 }
@@ -36,22 +36,79 @@ var bracketWest = {
   ],
   results: [
       [
-          [[0, 1,'Match 1'], [2, 3, 'Match 2'], [4, 5, 'Match 3'], [6, 7, 'Match 4']],
+          [[0, 1,'Match 1'], [2, 1, 'Match 2'], [4, 5, 'Match 3'], [6, 7, 'Match 4']],
           [[8, 9,'Central Division Final'], [10, 11, 'Pacific Division Final']],
-          [[12, 13, 'Western Conference Final'], [null, null, 'game']]
+          [[12, 13, 'Western Conference Final']]
       ]
   ]
 }
  
+/* Render function is called for each team label when data is changed, data
+ * contains the data object given in init and belonging to this slot.
+ *
+ * 'state' is one of the following strings:
+ * - empty-bye: No data or score and there won't team advancing to this place
+ * - empty-tbd: No data or score yet. A team will advance here later
+ * - entry-no-score: Data available, but no score given yet
+ * - entry-default-win: Data available, score will never be given as opponent is BYE
+ * - entry-complete: Data and score available
+ */
+function edit_fn(container, data, doneCb) {
+  var input = $('<input type="text">')
+  input.val(data ? data : '')
+  //container.html(input)
+  //input.focus()
+  input.blur(function() {
+    var inputValue = input.val()
+    if (inputValue.length === 0) {
+      doneCb(null); // Drop the team and replace with BYE
+    } else {
+      var flagAndName = inputValue.split(':') // Expects correct input
+      doneCb(inputValue)
+    }
+  })
+}
  
+
+function render_fn(container, data, score, state) {
+  switch(state) {
+    case "empty-bye":
+      container.append("No team")
+      return;
+    case "empty-tbd":
+      container.append("Upcoming")
+      return;
+ 
+    case "entry-no-score":
+    case "entry-default-win":
+    case "entry-complete":
+      container.append('<img src="2017/teams/'+data+'/'+data+'logo.png" height="100%" ></img> ').append(data)
+      return;
+  }
+}
+
+
 $(function() {
-    $('#demo').bracket({
+    $('#westBracket').bracket({
 	skipConsolationRound: true,
-	init: bracketWest /* data to initialize the bracket with */ })
-    $('#demo2').bracket({
+	disableTeamEdit: true,
+	disableToolbar: true,
+	init: bracketWest,
+	save: function(){},
+	decorator: {edit: edit_fn,
+		    render: render_fn}
+    })
+    $('#eastBracket').bracket({
 	skipConsolationRound: true,
 	dir: 'rl',
-	init: bracketEast /* data to initialize the bracket with */ })
+	disableTeamEdit: true,
+	disableToolbar: true,
+	init: bracketEast,
+	save: function(){},
+	decorator: {edit: edit_fn,
+	    render: render_fn}
+    })
+    $('.score').off('click')
 });
 
 
