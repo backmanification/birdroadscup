2b8700125b6347165ed8c68e3ffebab99a062d29
Filip <fbackman@cern.ch>
2018-07-10 00:09:03 +0200

    draft fixing continues

--- 2018/birdroadscup.py
+++ 2018/birdroadscup.py
@@ -196,7 +196,7 @@ def show_draft():
                 inprog = item.split(',')
                 dbl[inprog[0]] = inprog
     except:
-        continue
+        True
     solodrafttext = ''
     for i in range(1,len(solo)+1):
         solodrafttext += '<div class="row"><div class="col-xs-4"><p class="finalscore">'+solo[str(i)][0]+'</p></div><div class="col-xs-4"><h2>'+solo[str(i)][3]+'</h2><h3>'+solo[str(i)][4]+'</h3></div><div class="col-xs-4"><img src="/static/teams/'+solo[str(i)][2]+'/'+solo[str(i)][2]+'logo.png" width="100%" ></img></div></div>'
