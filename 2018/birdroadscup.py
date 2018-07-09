ea2a924cb84e8a359ca7ebb25c87d7c10fb15e32
Filip <fbackman@cern.ch>
2018-07-10 00:01:46 +0200

    draft fixing continues

--- 2018/birdroadscup.py
+++ 2018/birdroadscup.py
@@ -180,17 +180,23 @@ def show_awards():
 @app.route('/draft/')
 def show_draft():
     infofile = open('static/draft.txt').read()
-    infosolodbl = infofile.split('dubbel\n')
+    try:
+        infosolodbl = infofile.split('dubbel\n')
+        dbl = {}
+    except:
+        infosolodbl = [infofile]
     solo = {}
-    dbl = {}
     for item in infosolodbl[0].split('\n'):
         if item != '':
             inprog = item.split(',')
             solo[inprog[0]] = inprog
-    for item in infosolodbl[1].split('\n'):
-        if item != '':
-            inprog = item.split(',')
-            dbl[inprog[0]] = inprog
+    try:
+        for item in infosolodbl[1].split('\n'):
+            if item != '':
+                inprog = item.split(',')
+                dbl[inprog[0]] = inprog
+    except:
+        continue
     solodrafttext = ''
     for i in range(1,len(solo)+1):
         solodrafttext += '<div class="row"><div class="col-xs-4"><p class="finalscore">'+solo[str(i)][0]+'</p></div><div class="col-xs-4"><h2>'+solo[str(i)][3]+'</h2><h3>'+solo[str(i)][4]+'</h3></div><div class="col-xs-4"><img src="/static/teams/'+solo[str(i)][2]+'/'+solo[str(i)][2]+'logo.png" width="100%" ></img></div></div>'
